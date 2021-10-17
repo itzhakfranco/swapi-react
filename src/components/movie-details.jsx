c5668d89bef2c32f3a228e36c0e0182543793168
Itzhak Franco <itzhakfranco@gmail.com>
2021-10-17 23:38:48 +0300

    added custom msg in the event no movie was selected

--- src/components/movie-details.jsx
+++ src/components/movie-details.jsx
@@ -22,44 +22,56 @@ const MovieDetails = ({ movieDetail, favoritsMovies, setFavoritsMovies }) => {
 		setFavoritsMovies(updatedFavorites);
 	};
 
-	return (
-		<div className='col-md-8'>
-			<Header title='Movie Detailes Section' />
-			<div className='card'>
-				<div className='card-header'>
-					Directer: {movieDetail.director}
-					{isFavorite && (
-						<button
-							onClick={addToFavorites}
-							className='btn btn-sm btn-dark float-end'
-						>
-							<i className='fas fa-bookmark me-2'></i>
-							Add to Favories
-						</button>
-					)}
-					{!isFavorite && (
-						<button
-							onClick={removeFromFavorites}
-							className='btn btn-sm btn-danger float-end'
-						>
-							<i className='fas fa-trash-alt me-2'></i>
-							Remove from Favories
-						</button>
-					)}
-				</div>
-				<div className='card-body'>
-					<div style={{ display: "flex", alignItems: "baseline" }}>
-						<i className='fas fa-video float me-2'></i>
-						<h5 className='card-title'>{movieDetail.title}</h5>
-					</div>
-					<p className='card-text'>{movieDetail.opening_crawl}</p>
-				</div>
-				<div className='card-footer text-muted'>
-					<i className='fas fa-calendar-alt me-2'></i>
-					Release Date: {movieDetail.release_date}
-				</div>
+	const renderNoMovieSelected = () => {
+		return (
+			<div className='alert alert-secondary' role='alert'>
+				Click on a movie, In Order to see the movie's detailes
 			</div>
-		</div>
+		);
+	};
+
+	return (
+		<>
+			<Header title='Movie Detailes Section' />
+			{Object.keys(movieDetail).length === 0 ? (
+				renderNoMovieSelected()
+			) : (
+				<div className='card'>
+					<div className='card-header'>
+						Directer: {movieDetail.director}
+						{isFavorite && (
+							<button
+								onClick={addToFavorites}
+								className='btn btn-sm btn-dark float-end'
+							>
+								<i className='fas fa-bookmark me-2'></i>
+								Add to Favories
+							</button>
+						)}
+						{!isFavorite && (
+							<button
+								onClick={removeFromFavorites}
+								className='btn btn-sm btn-danger float-end'
+							>
+								<i className='fas fa-trash-alt me-2'></i>
+								Remove from Favories
+							</button>
+						)}
+					</div>
+					<div className='card-body'>
+						<div style={{ display: "flex", alignItems: "baseline" }}>
+							<i className='fas fa-video float me-2'></i>
+							<h5 className='card-title'>{movieDetail.title}</h5>
+						</div>
+						<p className='card-text'>{movieDetail.opening_crawl}</p>
+					</div>
+					<div className='card-footer text-muted'>
+						<i className='fas fa-calendar-alt me-2'></i>
+						Release Date: {movieDetail.release_date}
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
--- src/components/movie-list.jsx
+++ src/components/movie-list.jsx
@@ -3,9 +3,8 @@ import Header from "./header";
 
 const MovieList = ({ moviesData, setMovieDetail, movieDetail }) => {
 	return (
-		<div className='col-md-4'>
+		<>
 			<Header title='Star Wars Movies' />
-
 			<div className='list-group list-group-flush border-bottom scrollarea'>
 				{moviesData.map((movie) => (
 					<MovieItem
@@ -16,7 +15,7 @@ const MovieList = ({ moviesData, setMovieDetail, movieDetail }) => {
 					/>
 				))}
 			</div>
-		</div>
+		</>
 	);
 };
 
